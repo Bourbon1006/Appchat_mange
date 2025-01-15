a105f512a300381f13b6336e1b56f120e34851e5
Administrator <2830446056@qq.com>
2025-01-15 11:33:59 +0800

    init

--- src/main/java/org/example/appchat_hanlder/AppchatHanlderApplication.java
+++ src/main/java/org/example/appchat_hanlder/AppchatHanlderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppchatHanlderApplication {
+    //1234342
     public static void main(String[] args) {
         SpringApplication.run(AppchatHanlderApplication.class, args);
     }
